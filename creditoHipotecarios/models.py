ea608cc7f8b604e68d3ee148bcbd668bdc68b4e2
Jorge Arteaga <jorgelarteagap@gmail.com>
2021-01-10 18:33:08 -0400

    Second commit

--- creditoHipotecarios/models.py
+++ creditoHipotecarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class CreditoHipotecario(models.Model):
+
+    Banco = models.CharField(max_length=50)
+    
+    class Meta:
+        verbose_name = ("CreditoHipotecario")
+        verbose_name_plural = ("CreditoHipotecarios")
+
+    def __str__(self):
+        return self.name
