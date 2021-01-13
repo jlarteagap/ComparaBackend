de03bd03b67905b32ed6cf3803353260b36c758d
Jorge Arteaga <jorgelarteagap@gmail.com>
2021-01-13 16:20:57 -0400

    errores

--- Instituciones/models.py
+++ Instituciones/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class InstitucionesFinancieras(models.Model):
 
     name = models.CharField(max_length=50)
-    logo = models.ImageField()
+    logo = models.ImageField(null = True, blank = True)
 
     class Meta:
         verbose_name = ("Instituciones Financiera")
